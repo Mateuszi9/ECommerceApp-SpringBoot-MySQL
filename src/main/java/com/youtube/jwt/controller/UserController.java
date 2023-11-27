e53f49e37fa99963222440fd4209433e9adf23fb
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-27 11:27:41 +0100

    Add to pom dependencies for security

--- src/main/java/com/youtube/jwt/controller/UserController.java
+++ src/main/java/com/youtube/jwt/controller/UserController.java
@@ -3,6 +3,7 @@ package com.youtube.jwt.controller;
 import com.youtube.jwt.entity.User;
 import com.youtube.jwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,15 @@ public class UserController {
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    public String forAdmin() {
+        return "This URL is only for admin!";
+    }
+
+    @GetMapping({"/forUser"})
+    public String forUser() {
+        return "This URL is only for user!";
     }
 }
