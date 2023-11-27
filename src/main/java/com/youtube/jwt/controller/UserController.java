5d53a950737c1076bb8eee4b0a723b20de19b522
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-27 11:19:57 +0100

    Add default data to user and role entity

--- src/main/java/com/youtube/jwt/controller/UserController.java
+++ src/main/java/com/youtube/jwt/controller/UserController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostConstruct
+    public void initRolesAndUsers() {
+        userService.initRolesAndUser();
+    }
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
