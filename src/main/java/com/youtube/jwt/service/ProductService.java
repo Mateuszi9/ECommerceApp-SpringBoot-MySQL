91d76f6bac6c236d54916595641f425aa22d78bf
Mateusz Izdebski <mati2k51@gmail.com>
2023-11-28 12:04:18 +0100

    Add new Entity, Service, Controller and Dao for Product model

---
+++ src/main/java/com/youtube/jwt/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.youtube.jwt.service;
+
+import com.youtube.jwt.dao.ProductDao;
+import com.youtube.jwt.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+
+
+
+    public Product addNewProduct(Product product) {
+        return productDao.save(product);
+    }
+
+}
